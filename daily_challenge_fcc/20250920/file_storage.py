6fa2af23834f1d801e7fad82ff0e4387764af71e
Tomislav Dukez <tomdu3@ymail.com>
2025-09-21 07:46:45 +0100

    add comments

--- daily_challenge_fcc/20250920/file_storage.py
+++ daily_challenge_fcc/20250920/file_storage.py
@@ -1,8 +1,13 @@
 def number_of_files(file_size, file_unit, drive_size_gb):
+    """Calculate the number of files that can fit on a drive."""
+    
+    # Define conversion factors
     UNITS = {
         'B': 1,
         'KB': 1000,
         'MB': 1000000,
         'GB': 1000000000,
     }
+
+    # Calculate drive size in bytes and return the number of files that fit
     return (drive_size_gb * UNITS['GB'])//(file_size*UNITS[file_unit])
--- daily_challenge_fcc/20250921/video_storage.py
+++ daily_challenge_fcc/20250921/video_storage.py
@@ -8,9 +8,12 @@ def number_of_videos(video_size, video_unit, drive_size, drive_unit):
         'GB': 1000000000,
         'TB': 1000000000000
     }
+    
+    # Validate units
     if video_unit not in ['B', 'KB', 'MB', 'GB']:
         return 'Invalid video unit'
     if drive_unit not in ['GB', 'TB']:
         return 'Invalid drive unit'
 
+    # Calculate number of videos that can fit in the drive
     return (drive_size * UNITS[drive_unit])//(video_size * UNITS[video_unit])
