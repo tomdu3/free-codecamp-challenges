8eb246a3647859413972051e0d122fd6716ad51a
Tomislav Dukez <tomdu3@ymail.com>
2025-11-21 09:11:51 +0000

    fix remove print statement

--- daily_challenge_fcc/2025-11/25-11-21/lcm.py
+++ daily_challenge_fcc/2025-11/25-11-21/lcm.py
@@ -24,10 +24,7 @@ def check_factor(x, factor):
 def lcm(a, b):
     factors_a = [factor for factor in prime_factors(a) for _ in range(check_factor(a, factor))]
     factors_b = [factor for factor in prime_factors(b) for _ in range(check_factor(b, factor))]
-    print(factors_a)
-    print(factors_b)
     common_factors = set(factors_a + factors_b)
-    print(common_factors)
     lcm = 1
     for factor in common_factors:
         lcm *= factor ** max(factors_a.count(factor), factors_b.count(factor))
