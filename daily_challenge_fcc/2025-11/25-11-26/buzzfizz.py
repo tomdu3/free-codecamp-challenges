88d1223fd3783e29a8c37d3a3e3509e11ecffb80
Tomislav Dukez <tomdu3@ymail.com>
2025-11-26 08:54:45 +0000

    make better fizz_buzz

--- daily_challenge_fcc/2025-11/25-11-26/buzzfizz.py
+++ daily_challenge_fcc/2025-11/25-11-26/buzzfizz.py
@@ -7,16 +7,12 @@ def fizz_buzz(n):
 
     for i in range(1, n + 1):
         temp = ''
-        res = i
         for k, v in cases.items():
             if i % k == 0:
                 temp += v
-        if temp:
-            res = temp
-        result.append(res)
+        result.append(temp or i)
     return result
 
-def is_fiz_buzz(arr):
+def is_fizz_buzz(arr):
     result = fizz_buzz(len(arr))
     return arr == result
-    
