1270d4946de624b29fe79ea3bf14382e9d602618
Tomislav Dukez <tomdu3@ymail.com>
2025-12-29 07:36:28 +0000

    fix test for 25-12-19

--- daily_challenge_fcc/2025-12/25-12-19/test_pairwise.py
+++ daily_challenge_fcc/2025-12/25-12-19/test_pairwise.py
@@ -10,7 +10,7 @@ Waiting: 3. pairwise([-30, -15, 5, 10, 15, -5, 20, -40], -20) should return 22.
 Waiting: 4. pairwise([7, 9, 13, 19, 21, 6, 3, 1, 4, 8, 12, 22], 24) should return 10.
 """
 
-class TestPairwise(unittest.Testcase):
+class TestPairwise(unittest.TestCase):
     def test_positive_numbers(self):
         self.assertEqual(pairwise([2, 3, 4, 6, 8], 10), 9)
         self.assertEqual(pairwise([4, 1, 5, 2, 6, 3], 7), 15)
@@ -19,5 +19,5 @@ class TestPairwise(unittest.Testcase):
         self.assertEqual(pairwise([-30, -15, 5, 10, 15, -5, 20, -40], -20), 22)
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     unittest.main()
