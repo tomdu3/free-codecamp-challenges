43beb7616b4f06d202e7ed8306907b3594463cc9
Tomislav Dukez <tomdu3@ymail.com>
2026-08-05 22:43:12 +0100

    refactor solution for multiple3or5

--- project-euler/problem-01/multiplies3Or5.js
+++ project-euler/problem-01/multiplies3Or5.js
@@ -1,11 +1,11 @@
-function multiplesOf3Or5(number) {
-	const nums = [];
-	for (let i = 1; i < number; i++) {
-		if (i % 3 === 0 || i % 5 === 0) {
-			nums.push(i);
-		}
-	}
-	return nums.reduce((acc, curr) => acc + curr, 0);
+function multiplesOf3Or5(limit) {
+  let sumOfMultiples = 0;
+  for (let i = 1; i < limit; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sumOfMultiples += i;
+    }
+  }
+  return sumOfMultiples;
 }
 
 module.exports = multiplesOf3Or5;
